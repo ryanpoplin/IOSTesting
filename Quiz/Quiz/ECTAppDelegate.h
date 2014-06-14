bcaf4e8328c23c610e37233a29b68dae48c13644
Byrdann Fox <mobileupstate@gmail.com>
2014-06-14 08:24:12 -0400

    Upadtes...

---
+++ Quiz/Quiz/ECTAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  ECTAppDelegate.h
+//  Quiz
+//
+//  Created by Byrdann Fox on 6/14/14.
+//  Copyright (c) 2014 ExcepApps. All rights reserved.
+//
+
+// ECTAPPDELEGATE CLASS...
+// CLASSES DEFINE A KIND OF OBJECT...
+// AN INSTANCE OF THIS CLASS WILL BE CREATED...
+// MVC: MODEL OBJECT, VIEW OBJECT, OR CONTROLLER OBJECT...
+// 
+
+#import <UIKit/UIKit.h>
+
+@interface ECTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Quiz/Quiz/ECTQuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  ECTQuizViewController.h
+//  Quiz
+//
+//  Created by Byrdann Fox on 6/14/14.
+//  Copyright (c) 2014 ExcepApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ECTQuizViewController : UIViewController
+
+@end
