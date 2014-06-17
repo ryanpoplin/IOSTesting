9c81bff9aba4bbb9af0b254b7d3ac73a4097941b
Byrdann Fox <mobileupstate@gmail.com>
2014-06-16 22:23:43 -0400

    Updates...

---
+++ Prototype/Prototype/ECTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ECTAppDelegate.h
+//  Prototype
+//
+//  Created by Byrdann Fox on 6/16/14.
+//  Copyright (c) 2014 ExcepApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ECTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
